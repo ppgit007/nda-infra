5093cc333173cc328de29c748bf4b976853e8567
pp <pp@pp.com>
2026-06-22 18:27:45 +0530

    updated

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -10,41 +10,6 @@ variable "staticWebAppLocation" {
   default     = "East US 2"
 }
 
-variable "subscription_shortcut" {
-  description = "Subscription shortcut code for resource naming (e.g., vss)"
-  type        = string
-}
-
-variable "project" {
-  description = "Project name to be used in resource naming"
-  type        = string
-}
-
-variable "project_shortcut" {
-  description = "Project shortcut code for resource naming (e.g., nda, nba)"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name (e.g., dev, staging, prod)"
-  type        = string
-}
-
-variable "location_shortcut" {
-  description = "Location shortcut code for resource naming (e.g., eus for eastus, wus for westus)"
-  type        = string
-}
-
-variable "component" {
-  description = "Component name to be used in resource naming"
-  type        = string
-}
-
-variable "owner" {
-  description = "Owner of the resources"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
